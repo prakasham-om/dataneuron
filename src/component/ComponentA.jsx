0e59b381abfa1cb2aa7c954db3564a51073f6179
prakasham-om <prakashchandrasahoo8991@gmail.com>
2024-03-07 11:59:00 +0530

    hello

--- src/component/ComponentA.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComponentA = () => {
-  return (
-    <div className='bg-red-800 h-full w-full py-[5rem] '>ComponentA</div>
-  )
-}
-
-export default ComponentA
--- src/component/ComponentC.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CopmonentC = () => {
-  return (
-    <div>CopmonentC</div>
-  )
-}
-
-export default CopmonentC
